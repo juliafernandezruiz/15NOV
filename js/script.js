1d3c94d86b1be3839a80b172463db01e12147b43
DAFG-Works <140922558+DAFG-Works@users.noreply.github.com>
2024-11-15 19:41:03 +0100

    Arreglo primer conflicto

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
-  document.getElementById('contactForm').addEventListener('submit', function (e) {
+  document.getElementById('contactForm').addEventListener('submit'), function (e) {
     e.preventDefault(); // Prevenir el envío por defecto
     alert('Formulario enviado correctamente. ¡Gracias por tu mensaje!');
     this.reset(); // Reinicia el formulario
+  }
