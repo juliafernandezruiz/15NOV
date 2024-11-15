1acc4dc76180c80ae1e30182563de9459b5ef617
juliafernandezruiz <juliafernandezruiz03@gmail.com>
2024-11-15 19:18:22 +0100

    contact us

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log (holi)
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevenir el envío por defecto
+    alert('Formulario enviado correctamente. ¡Gracias por tu mensaje!');
+    this.reset(); // Reinicia el formulario
+});
